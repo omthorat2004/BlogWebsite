2af7a2e5d0f67841dae5f9bd18d17619d08bb6e5
om thorat <omthorat1005@gmail.com>
2024-01-03 17:19:28 -0800

    authorProfile started

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Author from './features/Author/Components/Author'
+import AuthorProfile from './features/Author/Components/AuthorProfile'
 import Home from './features/Home/Components/Home'
 import UserProfile from './features/UserProfile/Components/UserProfile'
 function App() {
@@ -14,8 +15,9 @@ function App() {
      <Router>
       <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/author' element={<Author/>}/>
+          <Route path='/authors' element={<Author/>}/>
           <Route path='/profile' element={<UserProfile/>}/>
+          <Route path='/author/1' element={<AuthorProfile/>}/>
       </Routes>
      </Router>
      
---
+++ Client/src/features/Author/Components/AuthorProfile.jsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mui/material';
+import React from 'react';
+
+const AuthorProfile = () => {
+  return (
+    <Stack direction='column'>
+        
+    </Stack>
+  );
+}
+
+export default AuthorProfile;
